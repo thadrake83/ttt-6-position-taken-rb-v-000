b699655753a319c5100fd2d18633c60ee3035c65
DRAKE RENTROP <drakerentrop@gmail.com>
2018-08-28 16:07:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def position_taken?(board, index)
+ !  (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  
+end
+  
